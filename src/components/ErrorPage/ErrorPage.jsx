e003b67263a7e16071935b5773b6e959308239cc
Sangma83 <poushalisangma13@gmail.com>
2024-03-26 22:22:54 +0600

    Header route created done

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Oppppppps!!</h2>
+            <Link to='/'>Go back Home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
