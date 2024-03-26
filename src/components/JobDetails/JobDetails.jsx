5df04dce08d652b16fd7fa0006b301e6dd8d1149
Sangma83 <poushalisangma13@gmail.com>
2024-03-27 03:14:04 +0600

    filtering done all, remote and onsite category

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../utility/localStorage";
 
 
 const JobDetails = () => {
@@ -11,6 +12,7 @@ const JobDetails = () => {
     console.log(job);
 
     const handleApplyJob = () =>{
+        saveJobApplication(idInt);
         toast("You have applied successfully!!");
     }
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -23,12 +23,13 @@ const router = createBrowserRouter([
       },
       {
         path: "/applied",
-        element: <AppliedJobs></AppliedJobs>
+        element: <AppliedJobs></AppliedJobs>,
+        loader: () => fetch('../jobs.json') //do not load all data only load the data you need
       },
       {
         path: '/job/:id',
         element: <JobDetails></JobDetails>,
-        loader: () => fetch('../jobs.json')
+        loader: () => fetch('../jobs.json')  //same warning
       }
     ]
   },
